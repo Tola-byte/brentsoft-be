757db84d3d63f589b9148b4aafb62d54586622d4
Tola-byte <adetolaisaiah17@gmail.com>
2024-05-21 10:58:44 +0100

    chore: add port

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,12 @@ import express from 'express';
 import emailRoutes from './routes/emailRoutes';
 
 const app = express();
+let PORT = 8000
 app.use(express.json());
 app.use('/api/email', emailRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
 
 export default app;
